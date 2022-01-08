67adeb284d969991ffb6ff5d7874776c492ba342
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-01-08 06:28:46 +0300

    photo-model

--- gallory/models.py
+++ gallory/models.py
@@ -13,3 +13,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+class Photo(models.Model):
+    class Meta:
+        verbose_name = 'Photo'
+        verbose_name_plural = 'Photos'
+    
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description        
